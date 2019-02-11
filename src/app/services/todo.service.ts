eef3398d2faba3e018062df8a5968b53a7d9cb5b
Dylan Attal <attal.dylan@gmail.com>
2019-02-11 17:12:54 -0500

    Toggle completed for data from server

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 import { Todo } from '../models/Todos'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +19,14 @@ export class TodoService {
 
   constructor(private http: HttpClient) {}
 
+  // Get todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`)
+  }
+
+  // Toggle completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`
+    this.http.put(url, todo, httpOptions)
   }
 }
